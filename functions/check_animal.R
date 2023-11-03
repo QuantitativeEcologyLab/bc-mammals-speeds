b4a4f081d234e8bccdaf4c6dd7b5ef2fb475cc60
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-11-03 09:01:12 -0700

    use `...` efficiently

--- functions/check_animal.R
+++ functions/check_animal.R
@@ -1,10 +1,10 @@
 source('functions/outlier_plots.R')
 
 #' function to run `outlie_plots()` for a given animal
-check_animal <- function(id, out = NULL, ci_level = 0, ...) {
+check_animal <- function(id, ...) {
   tel <- filter(d, animal == id) %>%
     pull(tel) %>%
     first() %>%
     as.telemetry(mark.rm = TRUE)
-  outlier_plots(tel, return = TRUE, ci_level = ci_level, ...)
+  outlier_plots(tel, return = TRUE, ...)
 }
