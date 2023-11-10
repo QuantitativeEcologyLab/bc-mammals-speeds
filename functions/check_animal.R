9a3b6363b8cbec9e002746ae1c890681e9912966
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-11-10 08:43:13 -0800

    add return_out argument

--- functions/check_animal.R
+++ functions/check_animal.R
@@ -1,10 +1,10 @@
 source('functions/outlier_plots.R')
 
 #' function to run `outlie_plots()` for a given animal
-check_animal <- function(id, ...) {
+check_animal <- function(id, return_out = TRUE, ...) {
   tel <- filter(d, animal == id) %>%
     pull(tel) %>%
     first() %>%
     as.telemetry(mark.rm = TRUE)
-  outlier_plots(tel, return = TRUE, ...)
+  outlier_plots(tel, return = return_out, ...)
 }
