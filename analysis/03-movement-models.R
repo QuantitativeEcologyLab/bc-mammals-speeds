e002f5c1a5741bc8881e8b4d47ae16f62197277b
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2023-11-18 11:20:15 -0800

    wrote code to fit movement models and AKDEs

---
+++ analysis/03-movement-models.R
@@ -0,0 +1,38 @@
+library('dplyr') # for data wrangling (mutate(), %>%, etc.)
+library('tidyr') # for data wrangling (unnest(), pivot_*, etc.)
+library('purrr') # for functional programming (map_***(), etc.)
+library('ctmm')  # for movement models
+
+#' `https://github.com/ctmm-initiative/ctmmlearn/blob/main/ctmm_error.R`
+# goats are the only species with calibration data
+goat_uere <-
+  as.telemetry('data/tracking-data/goat-calibration-data.csv') %>%
+  uere.fit()
+
+d <-
+  readRDS('data/tracking-data/all-tracking-data-cleaned-2023-11-17-16-04.rds') %>%
+  head(2) %>%
+  mutate(
+    # convert data frames to telemetry objects
+    tel = map(tel, \(x) as.telemetry(x, mark.rm = TRUE)),
+    # add calibration-informed error or a reasonable guess
+    tel = if_else(species == 'Oreamnos americanus',
+                  map(tel, \(x) {
+                    uere(x) <- goat_uere
+                    return(x)
+                  }),
+                  map(tel, \(x) {
+                    x$error <- 10
+                    return(x)
+                  })),
+    variogram = map(tel, \(x) ctmm.guess(data = x,
+                                         CTMM = ctmm(error = TRUE),
+                                         interactive = FALSE)),
+    movement_model = map(1:n(), \(i) ctmm.select(data = tel[[i]],
+                                                 CTMM = variogram[[i]]),
+                         .progress = 'Movement models'),
+    ud = map(1:n(), \(i) akde(data = tel[[i]], CTMM = movement_model,
+                              weights = TRUE),
+             .progress = 'UDs'))
+
+saveRDS(d, 'models/movement-models.rds')
