f9c5f01c9c4611a892ea0567fc92feac56be1072
Stefano Mezzini <42444204+StefanoMezzini@users.noreply.github.com>
2024-02-29 23:46:33 -0800

    added temporary slides; made map of all tels

--- analysis/figures/actws-2024-jasper/telemetry-map.R
+++ analysis/figures/actws-2024-jasper/telemetry-map.R
@@ -17,6 +17,7 @@ locs <- tibble(city = c('Kelowna', 'Jasper'),
               as.data.frame() %>%
               rename(alb_long = X, alb_lat = Y))
 
+# boreal datasets
 d <- readRDS('data/tracking-data/all-tracking-data-cleaned-2023-11-17-16-04.rds') %>%
   filter(dataset_name %in% c('Canis_lupus_boreal',
                              'Rangifer_tarandus_boreal')) %>%
@@ -63,6 +64,47 @@ ggplot() +
 # phylopics added manually using hex codes below
 color('bright')(2)
 
-ggsave('figures/actws-2024-jasper/telemetries-map.png',
+ggsave('figures/actws-2024-jasper/boreal-telemetries-map.png',
+       width = 33.87, height = 19.05, units = 'cm', dpi = 600,
+       bg = 'transparent')
+
+# all species
+d <- readRDS('data/tracking-data/all-tracking-data-cleaned-2023-11-17-16-04.rds') %>%
+  unnest(tel) %>%
+  filter(! outlier) %>%
+  select(location.long, location.lat, dataset_name, species) %>%
+  st_as_sf(coords = c('location.long', 'location.lat')) %>%
+  st_set_crs('+proj=longlat') %>%
+  st_transform('EPSG:3005') %>%
+  mutate(dataset_name = if_else(grepl('Puma_concolor', dataset_name),
+                                'Puma_concolor', dataset_name),
+         dataset_name =
+           factor(dataset_name,
+                  levels = c('Canis_lupus_boreal',
+                             'Rangifer_tarandus_boreal',
+                             'Rangifer_tarandus_southern_mountain',
+                             'Puma_concolor',
+                             'Elk in southwestern Alberta',
+                             'Oreamnos_americanus',
+                             'Ursus_arctos_horribilis')))
+
+# plot all datasets
+p_all <-
+  ggplot() +
+  theme_void() +
+  theme(legend.position = 'none') +
+  geom_sf(data = prov) +
+  geom_sf(aes(color = dataset_name), d) +
+  geom_sf(data = locs, size = 2) +
+  geom_label(aes(x = alb_long, y = alb_lat, label = city), locs,
+             nudge_x = 5e4, nudge_y = 5e4, label.size = 0, size = 6,
+             bg = 'transparent') +
+  coord_sf(xlim = c(2.5e5, 26e5), ylim = c(3.8e5, 18e5)) +
+  scale_color_manual('Species',
+                     values = c(as.character(khroma::color('bright')(6)),
+                                '#654321')) +
+  labs(x = NULL, y = NULL)
+
+ggsave('figures/actws-2024-jasper/telemetries-all-map.png', plot = p_all,
        width = 33.87, height = 19.05, units = 'cm', dpi = 600,
        bg = 'transparent')
